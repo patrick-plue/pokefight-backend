781c6f20a2544ed5b62d9f3cd597dc562dbf1a9a
Christian Löffler <cloeffle74@gmail.com>
2022-04-08 15:15:31 +0200

    start of implementing MongoDB for heighscores, added model, serverjs added app.post for highscore, tested made entries in DB

--- server.js
+++ server.js
@@ -2,6 +2,12 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+require('dotenv').config();
+const mongoose = require('mongoose');
+const Highscore = require('./models/highscoreSchema');
+
+//Mongoose connect to .env file
+mongoose.connect(process.env.MONGO_URL);
 
 // middlewares
 app.use(express.json());
@@ -10,6 +16,17 @@ app.use(
     origin: 'http://localhost:3000',
   })
 );
+// MongoDB Post for highscore, check if ID of pokemon exists in database if not create new entry, else update entry
+
+app.post('/highscore', (req, res) => {
+  Highscore.create({
+    id: req.body.id,
+    name: req.body.name,
+    playedgames: req.body.playedgames,
+    wongames: req.body.wongames,
+    lostgames: req.body.lostgames,
+  }).then((highscore) => res.send(highscore));
+});
 
 // config
 const port = process.env.PORT || 8000;
