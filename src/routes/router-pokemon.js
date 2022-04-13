0f8741153e8dc45135e31754a0c089b9a21533d1
Christian Löffler <cloeffle74@gmail.com>
2022-04-13 18:16:27 +0200

    after refactoring Highscore Routes

--- src/routes/router-pokemon.js
+++ src/routes/router-pokemon.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { route } = require('express/lib/router');
 const router = express.Router();
 
 const controller = require('../controller/controller-pokemon.js');
@@ -10,5 +11,15 @@ router.get('/types', controller.getDistinctTypes);
 router.get('/:id', controller.getPokemonById);
 
 router.get('/:id/:info', controller.getDetails);
+
+router.get('(/highscore', controller.getHighscore);
+
+router.get('/highscore/:id', controller.getHighscoreById);
+
+router.put('/highscore/:id/won', controller.putHighscoreWon);
+
+router.put('/highscore/:id/lost', controller.putHighscoreLost);
+
+router.delete('/highscore/:id', controller.deleteHighscore);
 
 module.exports = router;
